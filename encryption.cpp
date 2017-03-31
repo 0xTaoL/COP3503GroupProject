d0420348554f2b45989052f22796968277f598e9
Brady Ochse <bradyoch@gmail.com>
2017-03-31 10:08:57 -0400

    Improved encryption. Decryption is broken...

--- encryption.cpp
+++ encryption.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <stdexcept>
 #include <fstream>
+#include <cstdio>
 #include "encryption.h"
 
 using namespace std;
@@ -13,77 +14,63 @@ bool encryptor::validate_key(const string& user_key) const {
 }
 
 void encryptor::encrypt_file(const string& file_name) const {
-	fstream file(file_name, ios::binary | ios::in);
+	ifstream file(file_name, ios::binary | ios::in);
+        string tmp_name = file_name + ".tmp";
+        ofstream tmp_file(tmp_name, ios::binary | ios::out);
 	
 	if (!file.is_open()) {
-		file.close();
 		throw invalid_argument("File does not exist");
 	}
-	
+
+        for (size_t i = 0; i < key.length(); ++i) {
+                tmp_file.put(key[i] >> 1);
+        }
+        tmp_file.put('\n');
+
 	string contents;
-	while (file.good()) {
-		contents += file.get();
-	}
-	
-	if (file.bad()) {
-		file.close();
-		throw runtime_error("IO Error");
-	}
-	
+	while (getline(file, contents)) {
+                for (size_t i = 0; i < contents.length(); ++i) {
+                        tmp_file.put(contents[i] ^ key[i % key.length()]);
+                }
+        }
+        
 	file.close();
-	file.open(file_name, ios::trunc | ios::out | ios::binary);
-	
-	for (size_t i = 0; i < key.length(); ++i) {
-		file.put(key[i] >> 1);
-	}
-	
-	file.put('\n');
-	
-	for (size_t i = 0; i < contents.length() - 1; ++i) {
-		file.put(contents[i] ^ key[i % key.length()]);
-	}
-	
-	file.close();
+        tmp_file.close();
+        remove(file_name.c_str());
+        rename(tmp_name.c_str(), file_name.c_str());
 }
 
 void encryptor::decrypt_file(const string& file_name) const {
-	fstream file(file_name, ios::binary | ios::in);
-	
-	if (!file.is_open()) {
-		file.close();
-		throw invalid_argument("File does not exist");
-	}
-	
-	string hashed_key;
-	getline(file, hashed_key);
-	
-	if (key.length() == hashed_key.length()) {
-		for (size_t i = 0; i < key.length(); ++i) {
-			if ((key[i] >> 1) != hashed_key[i]) {
-				throw runtime_error("Password is invalid");
-			}
-		}
-	}
-	else {
-		throw runtime_error("Password is invalid");
-	}
-	
-	string contents;
-	while (file.good()) {
-		contents += file.get();
-	}
-	
-	if (file.bad()) {
-		file.close();
-		throw runtime_error("IO Error");
-	}
-	
-	file.close();
-	file.open(file_name, ios::trunc | ios::out | ios::binary);
-	
-	for (size_t i = 0; i < contents.length() - 1; ++i) {
-		file.put(contents[i] ^ key[i % key.length()]);
-	}
-	
-	file.close();
+	ifstream file(file_name, ios::binary | ios::in);
+        string tmp_name = file_name + ".tmp";
+        ofstream tmp_file(tmp_name, ios::binary | ios::out);
+
+        if (!file.is_open()) {
+                throw invalid_argument("File does not exist");
+        }
+
+        string test_key;
+        getline(file, test_key);
+        if (test_key.length() == key.length()) {
+                for (size_t i = 0; i < test_key.length(); ++i) {
+                        if ((key[i] >> 1) != test_key[i]) {
+                                throw invalid_argument("Incorrect Password");
+                        }
+                }
+        }
+        else {
+            throw invalid_argument("Incorrect Password");
+        }
+
+        string contents;
+        while (getline(file, contents)) {
+                for (size_t i = 0; i < contents.length(); ++i) {
+                        tmp_file.put(contents[i] ^ key[i % key.length()]);
+                }
+        }
+
+        file.close();
+        tmp_file.close();
+        remove(file_name.c_str());
+        rename(tmp_name.c_str(), file_name.c_str());
 }
