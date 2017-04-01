dd286c6b818557fd4d13af63fed9866c9abdd325
Brady Ochse <bradyoch@gmail.com>
2017-04-01 18:00:49 -0400

    basic command and text functionality added

--- text_editor.cpp
+++ text_editor.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <ncurses.h>
 #include "text_editor.h"
 
@@ -19,6 +18,7 @@ void text_editor::write_file() const {
 
 void text_editor::run_text_editor() {
 	string command = "";
+	string buffer = "";
 
 	initscr();
 
@@ -28,31 +28,31 @@ void text_editor::run_text_editor() {
 
 	int ch = 0;
 	while (ch = getch()) {
-		if (ch == 27) {
-			//turn on no delay to check if esc or alt
-			nodelay(stdscr, true);
+		unsigned int x, y;
+		getyx(stdscr, y, x);
 
-			//check if esc then go to command screen
-			if (getch() == -1 && command_prompt()) {
-				 break;
-			}
-
-			//turn off nodelay to wait for next keypress
-			nodelay(stdscr, false);
+		//check if esc then go to command screen
+		if (ch == 27 && command_prompt()) {
+			break;
 		}
 		else {
 			switch (ch) {
 			case KEY_UP:
+				--y;
 				break;
 			case KEY_RIGHT:
+				++x;
 				break;
 			case KEY_DOWN:
+				++y;
 				break;
 			case KEY_LEFT:
+				--x;
 				break;
 			}
 		}
 
+		move(y, x);
 		refresh();
 	}
 
@@ -60,11 +60,41 @@ void text_editor::run_text_editor() {
 }
 
 bool text_editor::command_prompt() {
-	return true;
+	unsigned int x_max, y_max;
+	getmaxyx(stdscr, y_max, x_max);
+
+	WINDOW* p_cmd = newwin(1, x_max - 1, y_max - 1, 0);
+	keypad(p_cmd, true);
+
+	char ch = 0;
+	string command = "";
+	while ((ch = wgetch(p_cmd)) != '\n') {
+		if (ch == 8 || ch == 127 || ch == 7) {
+			if (command.length() > 0) {
+				command.pop_back();
+			}
+		}
+		else {
+			command += ch;
+		}
+
+		wclear(p_cmd);
+		waddstr(p_cmd, command.c_str());
+		wrefresh(p_cmd);
+	}
+	
+	wclear(p_cmd);
+	wrefresh(p_cmd);
+	delwin(p_cmd);
+	
+	if (command.compare("quit") == 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
 void text_editor::print_help() const {
-	cout << "Commands:\n"
- 		 << "i    -- start input\n"
- 		 << "help -- this screen" << endl;
+	//TODO
 }
--- text_editor.h
+++ text_editor.h
@@ -7,16 +7,15 @@ class text_editor {
 	const std::string temp_file, save_file;
 	bool write_mode;
 
-        void read_file() const;
-        void write_file() const;
-        bool command_prompt();
+	void read_file() const;
+	void write_file() const;
+	bool command_prompt();
+	void print_help() const;
+	void save();
 	
 public:
 	text_editor(const std::string& save_file);
-	void issue_command();
 	void run_text_editor();
-	void print_help() const;
-	void save();
 };
 
 #endif
